99076de1a2240082f0bacad0e1b355f1a2f9666e
fonsecaa007 <henrique.fonseca11@etec.sp.gov.br>
2022-08-29 11:58:12 -0300

    tentei ate o fim

--- src/Controllers/ClientController.js
+++ src/Controllers/ClientController.js
@@ -13,11 +13,26 @@ export class ClientController{
        }
     }
 
-    static novoCliente(req, res){
-        const { preco , descricao } = req.body;
-        const Produtos = new Produtos(preco, descricao).Insert();
-        return res.status(200).json({
-            msg : 'Cadastrado com sucesso!'
-        });
+    static async novoCliente(req, res){
+        try {
+            const { id, preco, descricao } = req.body;
+            const novo_cliente = await new Produtos(id, preco, descricao).Insert();
+            return res.status(200).json(novo_cliente);
+        } catch (error) {
+            console.log("error controller: " + error)
+            return res.status(500).json(error)
+        }
+    }
+
+    static async alterarCliente(req, res){
+        try {
+            const { id, preco, descricao } = req.body;
+            const { id } = req.params;
+            const alt_cliente = await new Produtos(preco, descricao, id).Update();
+            return res.status(200).json(alt_cliente);
+        } catch (error) {
+            console.log("error controller: " + error)
+            return res.status(500).json(error)
+        }
     }
 }
--- src/Models/ClientModel.js
+++ src/Models/ClientModel.js
@@ -2,24 +2,39 @@ import { CreatePoolSqlServer } from "../../pool.js";
 const conexao = await CreatePoolSqlServer();
 
 export class Produtos{
-    constructor(preco, descricao){
+    constructor(preco, descricao, id){
         this._preco = preco,
         this._descricao = descricao
+        this._id = id
     }
 
     static async SelectAll(){
       try {
-        const { recordset } = await conexao.query('select * from Produtos')
-        return recordset
+          const { recordset } = await conexao.query('select * from Produtos')
+          return recordset
       } catch (error) {
-
+          console.log("error script ClientModel.js " + error)
+          return false
       }
-    }
+  }
 
-    Insert(){
-        ProdutosTable.push({
-            preco: this._preco,
-            descricao : this._descricao
-        })
-    }
+  async Insert(){
+     try {
+          const { rowsAffected } = await conexao.query(`insert into Produtos values('${ this._preco }',${ this._descricao },'${ this._id}')`)
+          return true
+      } catch (error) {
+          console.log("error model: " + error)
+          return false
+      }
+  }
+
+  async Update(){
+      try {
+          const { rowsAffected } = await conexao.query(`update Produtos set nome = '${this._preco}' , descricao = ${this._descricao},  where id = ${this._id}`)
+          return rowsAffected
+      } catch (error) {
+          console.log("error model: " + error)
+          return false
+      }
+  }
 }
